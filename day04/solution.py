0f91c076ff04ff7b9b40117614dc38c0c630a815
matt <mattdgraham95@gmail.com>
2022-12-05 10:35:33 -0500

    4-2

--- day04/solution.py
+++ day04/solution.py
@@ -8,14 +8,24 @@ def parseLine(line: str) -> Tuple[int, int, int, int]:
     y1, y2 = y.split("-")
     return (int(point) for point in (x1, x2, y1, y2))    
 
-def overlaps(x1: int, x2: int, y1: int, y2: int) -> bool:
+def overlaps_all(x1: int, x2: int, y1: int, y2: int) -> bool:
     return (x1 <= y1 and x2 >= y2) or (y1 <= x1 and y2 >= x2)
 
+def overlaps_any(x1: int, x2: int, y1: int, y2: int) -> bool:
+    return (
+        overlaps_all(x1, x2, y1, y2)
+        or (x1 <= y1 and y1 <= x2)
+        or (x1 >= y1 and x1 <= y2)
+    )
+
 with open("input.txt", "r") as f:
-    solution1 = 0
+    solution1, solution2 = 0, 0
     for line in f.readlines():
         x1, x2, y1, y2 = parseLine(line)
-        if overlaps(x1, x2, y1, y2):
+        if overlaps_all(x1, x2, y1, y2):
             solution1 += 1
+        if overlaps_any(x1, x2, y1, y2):
+            solution2 += 1 
 
 print(f"{solution1=}")
+print(f"{solution2=}")
